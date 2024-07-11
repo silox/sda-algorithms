23c9dd14656e609d0d536c6a3ded62af3a860134
silox <edruisl99@gmail.com>
2024-07-11 08:32:56 +0200

    Add heap task solution

---
+++ trees/heap_task_solution.py
@@ -0,0 +1,27 @@
+import heapq
+
+
+# Napis funkciu `find_k_smallest(array, k)`, ktora vrati zoznam `k` najmensich hodnot v zozname.
+def find_k_smallest(array, k):
+    array = array[:]
+    heapq.heapify(array)
+    result = []
+    for _ in range(k):
+        result.append(heapq.heappop(array))
+    return result
+
+
+def find_k_smallest_alternative(array, k):
+    return heapq.nsmallest(k, array)
+
+
+if __name__ == '__main__':
+    array1 = [1, 2, 3, 4, 5]
+    array2 = [5, 4, 3]
+    array3 = [3, 0, -1, -4, 50, 3, 6, 6, 4, 19, 20]
+    print(find_k_smallest(array1, 3))
+    print(find_k_smallest(array2, 3))
+    print(find_k_smallest(array3, 3))
+
+    print(find_k_smallest_alternative(array3, 5))
+    print(array3)
